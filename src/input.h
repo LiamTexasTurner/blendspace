81395afc10b710b58aec4f0fe1b93f32eb21bf5c
LiamTexasTurner <liamtexasturner@gmail.com>
2026-03-09 12:32:58 +1100

    and the input .h

---
+++ src/input.h
@@ -0,0 +1,35 @@
+void process_input(float dt, glm::vec2 &input)
+{
+      input = glm::vec2(0.0f);
+      if(IsKeyPressed(KEY_Q))
+      {
+            CloseWindow();
+      }
+
+      glm::vec2 input_vectors(0.0f);
+      if(IsKeyDown(KEY_E))
+      {
+            input_vectors += glm::vec2(0.0f, 1.0f);
+      }
+      if(IsKeyDown(KEY_F))
+      {
+            input_vectors += glm::vec2(1.0f, 0.0f);
+      }
+      if(IsKeyDown(KEY_D))
+      {
+            input_vectors += glm::vec2(0.0f, -1.0f);
+      }
+      if(IsKeyDown(KEY_S))
+      {
+            input_vectors += glm::vec2(-1.0f, 0.0f);
+      }
+
+      if(glm::length(input_vectors) > 0.5f)
+      {
+            input = glm::normalize(input_vectors);;
+      }
+      else
+      {
+            input = glm::vec2(0.0f);
+      }
+}
